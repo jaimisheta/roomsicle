47ddfffd48a04eea21e7c023487af68a6996a649
rikinp <rikin201@gmail.com>
2021-10-17 13:48:23 -0300

    Initial project create

---
+++ src/main/java/com/finder/roomsiclee/App.java
@@ -0,0 +1,13 @@
+package com.finder.roomsiclee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
