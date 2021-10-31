ae09509018c9f6f6e43d9c9d3c7fed9a4263571a
Sourav Malik <sr343164@dal.ca>
2021-10-31 03:58:31 -0300

    Added Configuration Properties File For Database Connection Class

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,22 +1,62 @@
 package database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-public class DatabaseConnection {
-    String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    String URL = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_24_DEVINT";
-    String USER = "CSCI5308_24_DEVINT_USER";
-    String PASSWORD = "pa6aechaLohd0aik";
+public class DatabaseConnection{
+    private static DatabaseConnection databaseConnection = null;
+    public static Connection connection = null;
+
+    private String JDBC_DRIVER;
+    private String URL;
+    private String USER;
+    private String PASSWORD;
+
+    public DatabaseConnection(){
+        Properties properties = new Properties();
+        try{
+            FileInputStream fileInputStream = new FileInputStream("config.properties");
+            properties.load(fileInputStream);
+            fileInputStream.close();
+            JDBC_DRIVER = properties.getProperty("jdbc_driver");
+            URL = properties.getProperty("url");
+            USER = properties.getProperty("user");
+            PASSWORD = properties.getProperty("password");
+        } catch(IOException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     public Connection getConnectionObject(){
-        Connection connection = null;
         try{
             Class.forName(JDBC_DRIVER);
             connection = DriverManager.getConnection(URL,USER, PASSWORD);
-        } catch(Exception e) {
+        } catch(Exception e){
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static DatabaseConnection databaseConnectionObject() {
+        if(databaseConnection == null){
+            databaseConnection = new DatabaseConnection();
+        }
+        return databaseConnection;
+    }
+
+    public static void closeDatabaseConnection(){
+        try{
+            if(connection!=null){
+                connection.close();
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
