61e4c5fa3bd0c68881e97b4cbb5f33466857af79
Sourav Malik <sr343164@dal.ca>
2021-11-23 19:18:25 -0400

    Configured log4j

--- src/main/java/RoommateFinder.java
+++ src/main/java/RoommateFinder.java
@@ -1,6 +1,11 @@
-import database.DatabaseConnection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RoommateFinder {
+
+    static final Logger logger = LogManager.getLogger(RoommateFinder.class);
+
     public static void main(String[] args) {
+        logger.info("Started Roomsicle Application");
     }
 }
