b63d7ac36cb69b4f32ffea582a3564bc593cff8a
Rikin <rk947420@dal.ca>
2021-12-08 23:17:21 -0400

    refactored code

--- src/main/java/controller/Navigator.java
+++ src/main/java/controller/Navigator.java
@@ -1,6 +1,9 @@
 package controller;
 
 import commandline.IRoomsicleCLI;
+import controller.userprofile.IUserHomePageController;
+
+import java.util.InputMismatchException;
 
 public class Navigator {
 
@@ -8,14 +11,26 @@ public class Navigator {
 
         IRoomsicleCLI roomsicleCLI;
         int userInput;
-
         roomsicleCLI = ClassInitializer.initializer().getRoomsicleCLI();
 
-        roomsicleCLI.printMessage("navigation.option");
-        userInput = roomsicleCLI.getNumberResponse();
+        try {
+            roomsicleCLI.printMessage("navigation.option");
+            userInput = roomsicleCLI.getNumberResponse();
 
-        if(userInput==1){
+            if (userInput == 1) {
+                IUserHomePageController userHomePageController;
+                userHomePageController = ClassInitializer.initializer().getIUserHomePageController();
 
+                userHomePageController.showUserHomePageController();
+            } else if (userInput == 2) {
+                System.exit(0);
+            } else {
+                roomsicleCLI.printMessage("invalid.entry.message");
+                navigator();
+            }
+        }catch(InputMismatchException e){
+            roomsicleCLI.printMessage("invalid.entry.message");
+            navigator();
         }
 
     }
